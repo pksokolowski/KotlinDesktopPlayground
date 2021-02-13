4fb25bc6767c83ea8d52db89973943e84842fafa
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2021-02-13 10:56:20 +0100

    display a dialog

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,11 @@
 import androidx.compose.desktop.Window
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.window.Dialog
 import feature.dependencies.Dependency
 import modules.mainModule
 import org.koin.core.context.startKoin
@@ -17,14 +17,47 @@ fun main() = Window {
     }
 
     var text by remember { mutableStateOf("Hello, World!") }
+    var showDialog by remember { mutableStateOf(false) }
 
     val dependency by inject(Dependency::class.java)
 
-    MaterialTheme {
-        Button(onClick = {
-            text = dependency.getString()
-        }) {
-            Text(text)
+    @Composable
+    @Suppress("FunctionName")
+    fun DialogButton() {
+        Row {
+            Text("Some label")
+            Button({
+                showDialog = true
+            }) {
+                Text("Click here!")
+            }
         }
     }
+
+    MaterialTheme {
+
+        Column {
+            DialogButton()
+
+            Button(onClick = {
+                text = dependency.getString()
+            }) {
+                Text(text)
+            }
+
+        }
+
+        if (showDialog) {
+            Dialog({
+                showDialog = false
+            }) {
+                Text("Dialog's content :)")
+                Button({ showDialog = false }) {
+                    Text("Hide dialog")
+                }
+            }
+        }
+    }
+
 }
+
