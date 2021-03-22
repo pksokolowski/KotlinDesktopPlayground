7edb0c7adad3afcbb5e22117ba882d87e9800e6d
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2021-03-22 23:20:32 +0100

    add alternative factorial sample

--- src/main/kotlin/features/coroutines/domain/samples/FactorialCoroutinesSample.kt
+++ src/main/kotlin/features/coroutines/domain/samples/FactorialCoroutinesSample.kt
@@ -1,8 +1,7 @@
 package features.coroutines.domain.samples
 
 import features.coroutines.domain.custom.groupN
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import java.math.BigInteger
 
@@ -41,10 +40,14 @@ class FactorialCoroutinesSample : CoroutinesSample(
                 productOf(products)
             }
             .onEach { result ->
-                output("result is $result")
+                output("\nresult is:\n$result")
             }
             .launchIn(scope)
 
+        scope.launch {
+            val alternativeResult = computeFactorial(number)
+            output("\nalternative solution's result is:\n$alternativeResult")
+        }
     }
 
     private fun productOf(integers: List<BigInteger>): BigInteger {
@@ -70,4 +73,41 @@ class FactorialCoroutinesSample : CoroutinesSample(
         }
     }
 
+    private suspend fun CoroutineScope.computeFactorial(number: BigInteger) = withContext(Dispatchers.Default) {
+        data class BigIntRange(val start: BigInteger, val end: BigInteger)
+
+        val processorsCount = Runtime.getRuntime().availableProcessors()
+
+        val concurrency = if (number < processorsCount.toBigInteger() * 2.toBigInteger()) 1 else processorsCount
+        val itemsPerWorker = number / concurrency.toBigInteger()
+        val remainder = number.remainder(concurrency.toBigInteger())
+        val expectedGroupsCount =
+            if (remainder == 0.toBigInteger()) concurrency else concurrency + 1
+
+        var rangeStart = 1.toBigInteger()
+        val rangesToMultiply = List<BigIntRange>(expectedGroupsCount) {
+            val unboundEnd = (rangeStart + itemsPerWorker - 1.toBigInteger())
+            val rangeEnd = unboundEnd.min(number)
+            val range = BigIntRange(rangeStart, rangeEnd)
+
+            rangeStart = rangeEnd + 1.toBigInteger()
+            range
+        }
+
+        rangesToMultiply.map { range ->
+            async {
+                var product = range.start
+                var i = range.start + 1.toBigInteger()
+                while (i <= range.end) {
+                    ensureActive()
+                    product *= i
+                    i++
+                }
+                product
+            }
+        }.awaitAll()
+            .fold(1.toBigInteger()) { acc, value ->
+                acc * value
+            }
+    }
 }
